8b1723b1736049ba7c28488b10abff285fa92bf1
Maxim Fesenko <mmfesenko@gmail.com>
2023-03-05 23:43:14 +0200

    The homework 2.1 is done

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 let userName = prompt('What is your name?');
-console.log(userName);
+let userSurname = prompt('What is your surname?');
+let userAge = prompt('How old are you?');
+let userCity = prompt('What city do you live in?');
+
+let personAge = +userAge;
+
+
+let user = {
+    name: `${userName}`,
+    surname: `${userSurname}`,
+    age: `${userAge}`,
+    city: `${userCity}`
+}
+
+let dataShare = confirm('Do you agree to share your personal data?');
+
+dataShare ? console.log(`Name: ${user.name}, Surname:${user.surname}, Age:${user.age}, City:${user.city}`) : alert('The user forbade showing data'); 
