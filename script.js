f4f4213f9b88d72f1abfefbec6ad5a1f97f9b829
Maxim Fesenko <mmfesenko@gmail.com>
2023-03-10 23:56:26 +0200

    all done in homework 3

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
-let userName = prompt('What is your name?');
+let getFirstUserNumber = prompt('Write a number:',100); // || 0
+let getSecondUserNumber = prompt('Write a number:',50); // || 22
+let getThirdUserNumber;
+let getFourthUserNumber;
 
-alert(`Hello, ${userName}! How are you?`);
+if (getFirstUserNumber === '') {
+    getFirstUserNumber = 0
+} 
+
+if (getSecondUserNumber === '') {
+    getSecondUserNumber = 22
+}
+
+if (getFirstUserNumber && getSecondUserNumber) {
+    getThirdUserNumber = 80
+} else if(getFirstUserNumber == false || getSecondUserNumber == false) {
+    getThirdUserNumber = 40
+}
+
+let object = {};
+
+if (getFirstUserNumber >= 90) {
+    object.size = 'big'
+} else if (getFirstUserNumber >= 40) {
+    object.size = 'small'
+} else {
+    object.size = 'medium'
+}
+
+switch(object.size) {
+    case 'big':
+        getFourthUserNumber = 1000;
+        break;
+    case 'medium':
+        getFourthUserNumber = 100;
+        break;
+    case 'small':
+        getFourthUserNumber = 10;
+        break;
+    default:
+        alert('Error!');
+}
+const multiplyUserNumbers = getFirstUserNumber * 
+getSecondUserNumber * 
+getThirdUserNumber * 
+getFourthUserNumber;
+
+const result = multiplyUserNumbers;
+
+if (result / 2 != 0) {
+    console.log('Your homework is done, dude!');
+} else if (getSecondUserNumber > 50 ) {
+    alert(getSecondUserNumber)
+}
+console.log(parseInt(getFirstUserNumber));
+console.log(parseInt(getSecondUserNumber));
+console.log(parseInt(getThirdUserNumber));
+console.log(object);
+console.log(parseInt(getFourthUserNumber));
+console.log(result);
