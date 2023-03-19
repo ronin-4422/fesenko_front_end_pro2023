2254a9bb862cb0dd20e85187d180bc2f1bef836c
Maxim Fesenko <mmfesenko@gmail.com>
2023-03-19 17:40:08 +0200

    lesson5

--- script.js
+++ script.js
@@ -1,83 +1,123 @@
-//1. Вивести в консоль в один рядок через кому числа від 10 до 20.
-let firstOutput = '';
+//1. Знайти суму та кількість позитивних елементів.
+const array = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47] 
 
-for (let i = 10; i <= 20; i++) {
-  firstOutput += i + ', ';
-}
+let sumPositive = 0;
+let countPositive = 0;
 
-console.log('----------------------1');
-console.log(firstOutput.slice(0,-2));
-
-//2.Вивести в консоль в один рядок через кому квадрати чисел від 10 до 20.
-let secondOutput = '';
-
-for (let i = 10; i <= 20; i++) {
-    secondOutput += i * i + ',';
-}
-
-console.log('----------------------2')
-console.log(secondOutput.slice(0,-2));
-
-//3.Знайти добуток усіх цілих чисел від 15 до 35.
-let thirdOutput = 1;
-
-for (let i = 15; i <= 35; i++) {
-    thirdOutput *= i;
-}
-
-console.log('----------------------3');
-console.log(thirdOutput);
-
-//4.Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
-let fourthOutput = 0;
-
-for (let i = 1; i <= 500; i++) {
-    fourthOutput += i;
-}
-
-const average = fourthOutput / 500;
-
-console.log('----------------------4');
-console.log(average);
-
-//5.Вивести суму лише парних чисел в діапазоні від 30 до 80.
-let fithOutput = 0;
-
-for (let i = 30; i <= 80; i++) {
-    if (i % 2 === 0) {
-        fithOutput += i;
-}
-}
-
-console.log('----------------------5');
-console.log(fithOutput);
-
-//6.Вивести всі числа в діапазоні від 100 до 200 кратні 3.
-let sixthOutput = '';
-
-for (let i = 100; i <= 200; i++) {
-  if (i % 3 === 0) {
-    sixthOutput += i + ', ';
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0) {
+    sumPositive += array[i];
+    countPositive++;
   }
 }
 
-console.log('----------------------6');
-console.log(sixthOutput.slice(0, -2));
+console.log('------------------1');
+console.log('Сума позитивних елементів:', sumPositive);
+console.log('Кількість позитивних елементів:', countPositive);
 
-//7.Дане ціле число. З'ясувати, чи є воно простим (простим називається число, більше 1, які не мають інших дільників крім 1 і себе).
-let seventhOutput = 18; //можно написать любое число
-let isPrime = true;
+//2. Знайти мінімальний елемент масиву та його порядковий номер.
+let min = array[0];
+let minIndex = 0;
 
-for (let i = 2; i < seventhOutput; i++) {
-    if (seventhOutput % i === 0) {
-      isPrime = false;
-      break;
-    }
+for (let i = 1; i < array.length; i++) {
+  if (array[i] < min) {
+    min = array[i]
+    minIndex = i
   }
+}
 
-  console.log('----------------------7');
-  if (isPrime) {
-    console.log(seventhOutput + " является простым числом");
-  } else {
-    console.log(seventhOutput + " не является простым числом");
+console.log('------------------2');
+console.log("Мінімальний елемент:", min);
+console.log("Його порядковий номер:", minIndex);
+
+//3. Знайти максимальний елемент масиву та його порядковий номер.
+let max = array[0];
+let maxIndex = 0;
+
+for (let i = 1; i < array.length; i++) {
+  if (array[i] > max) {
+    max = array[i]
+    maxIndex = i
   }
+}
+
+console.log('------------------3');
+console.log("Максімальний елемент:", max);
+console.log("Його порядковий номер:", maxIndex);
+
+//4. Визначити кількість негативних елементів.
+let negativeCount = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] < 0) {
+    negativeCount++;
+  }
+}
+
+console.log('------------------4');
+console.log("Кількість негативних елементів:", negativeCount);
+
+//5. Знайти кількість непарних позитивних елементів.
+let oddPositiveCount = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0 && array[i] % 2 !== 0) {
+    oddPositiveCount++;
+  }
+}
+
+console.log('------------------5');
+console.log("Кількість непарних позитивних елементів:", oddPositiveCount);
+
+//6. Знайти кількість парних позитивних елементів.
+let sumEvenPositive = 0;
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0 && array[i] % 2 !== 0) {
+    sumEvenPositive++;
+  }
+}
+console.log('------------------6');
+console.log("Кількість парних позитивних елементів: " + sumEvenPositive);
+
+//7. Знайти суму парних позитивних елементів.
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0 && array[i] % 2 === 0) {
+    sumEvenPositive += array[i];
+  }
+}
+
+console.log('------------------7');
+console.log("Сума парних позитивних елементів: " + sumEvenPositive);
+
+//8. Знайти суму непарних позитивних елементів.
+let sumOddPositive = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0 && array[i] % 2 !== 0) {
+    sumOddPositive += array[i];
+  }
+}
+
+console.log('------------------8');
+console.log("Сума непарних позитивних елементів: " + sumOddPositive);
+
+//9. Знайти добуток позитивних елементів.
+let arrayPositive = 1;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0) {
+    arrayPositive *= array[i];
+  }
+}
+
+console.log('------------------9');
+console.log("Добуток позитивних елементів: " + arrayPositive);
+
+//10. Знайти найбільший серед елементів масиву, остальні обнулити.
+for (let i = 0; i < array.length; i++) {
+  if (array[i] !== max) {
+    array[i] = 0;
+  }
+}
+
+console.log('------------------10');
+console.log("Масив з обнуленими елементами, крім максимального: " + array);
+
+
