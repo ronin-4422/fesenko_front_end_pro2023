5583cf21ab73490232a4e7b972573150d7845372
Maxim Fesenko <mmfesenko@gmail.com>
2023-03-26 19:30:08 +0300

    lesson7

--- script.js
+++ script.js
@@ -1,71 +1,54 @@
-let users = [
-  {
-  "index": 0,
-  "isActive": true,
-  "balance": "$2,226.60",
-  "name": "Eugenia Sawyer",
-  "gender": "female",
-  "phone": "+1 (840) 583-3207",
-  "address": "949 John Street, Rose, Puerto Rico, 1857"
-  },
-  {
-  "index": 1,
-  "isActive": true,
-  "balance": "$2,613.77",
-  "name": "Pauline Gallegos",
-  "gender": "female",
-  "phone": "+1 (985) 593-3328",
-  "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-  },
-  {
-  "index": 2,
-  "isActive": false,
-  "balance": "$3,976.41",
-  "name": "Middleton Chaney",
-  "gender": "male",
-  "phone": "+1 (995) 591-2478",
-  "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-  },
-  {
-  "index": 3,
-  "isActive": true,
-  "balance": "$1,934.58",
-  "name": "Burns Poole",
-  "gender": "male",
-  "phone": "+1 (885) 559-3422",
-  "address": "730 Seba Avenue, Osage, Alabama, 6290"
-  },
-  {
-  "index": 4,
-  "isActive": true,
-  "balance": "$3,261.65",
-  "name": "Mcfadden Horne",
-  "gender": "male",
-  "phone": "+1 (942) 565-3988",
-  "address": "120 Scholes Street, Kirk, Michigan, 1018"
-  },
-  {
-  "index": 5,
-  "isActive": false,
-  "balance": "$1,790.56",
-  "name": "Suzette Lewis",
-  "gender": "female",
-  "phone": "+1 (837) 586-3283",
-  "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+      i--;
+    }
   }
-  ]
+}
+removeElement(array, 3);
+console.log('----------1');
+console.log(array);
 
-  const phoneNumbers = users.filter(function(user) {
-    return parseInt(user.balance.replace("$", "").replace(",", "")) > 2000;
-  }).map(function(user) {
-    return user.phone;
-  });
-  
-  const totalBalance = users.reduce(function(accum, user) {
-    const balance = parseInt(user.balance.replace("$", "").replace(",", ""));
-    return accum + balance;
-  }, 0);
-  
-  console.log('Масив телефонних номерів користувачів, у яких баланс більше 2000 доларів:', phoneNumbers);
-  console.log('Cума всіх балансів користувачів:', totalBalance);
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+function generateKey(length, characters) {
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    key += characters[randomIndex];
+  }
+  return key;
+}
+const key = generateKey(10, characters);
+console.log('----------2');
+console.log(key);
+
+function removeChars(str, chars) {
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    if (!chars.includes(str[i])) {
+      result += str[i];
+    }
+  }
+  return result;
+}
+console.log('----------3');
+console.log(removeChars("hello world", ['l', 'd']));
+
+function createSum() {
+  let sum = 0;
+  return function(num) {
+    sum += num;
+    return sum;
+  };
+}
+
+const sum = createSum();
+
+console.log('----------4');
+console.log(sum(3)); 
+console.log(sum(5)); 
+console.log(sum(20)); 
